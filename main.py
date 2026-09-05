4b7b0418be85a0ae5bb1ba9dd36a518763bc74a1
Javed Mujawar <mujawar.javadu@gmail.com>
2026-09-05 23:31:35 +0530

    error handler

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 from pydantic import BaseModel
-from fastapi import FastAPI,status, HTTPException
+from fastapi import FastAPI,status, HTTPException, Request
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-@app.post("/user",status_code= status.HTTP_201_CREATED)
-def create_user():
-    return {"message": "User created successfully"}
+class UserNotFoundException(HTTPException):
+    def __init__(self,name:str):
+        self.name = name
 
-@app.get("/user")
-def get_user():
-    return {"message": "User retrieved successfully" , "status":"success"}
-@app.get("/use2/{user_id}")
-def get_user2(user_id: int):
+@app.exception_handler(UserNotFoundException)
+def handle_user_not_found(request: Request, exc: UserNotFoundException):
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content={"message": f"User '{exc.name}' not found.", "status":"error"},
+    )
+
+
+@app.get("/user/{name}")
+def get_user(name: str):
+    if name != "John":
+        raise UserNotFoundException(name)
+    return {name}
+        
+@app.get("/users/{user_id}")
+def get_users(user_id: int):
     if user_id != 1:
         raise HTTPException(status_code=404, detail="User not found")
-    return {"message": "User retrieved successfully" , "status":"success"}
+    return {"message": "User retrieved successfully"}
