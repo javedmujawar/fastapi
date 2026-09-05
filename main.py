e541c032b21b86420d08552f96dc8fb4c60cde84
Javed Mujawar <mujawar.javadu@gmail.com>
2026-09-05 12:55:50 +0530

    multiple routes

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Welcome to the FastAPI application."}
+
+#users route
+@app.get("/users")
+def users():
+    return {"users":["user1", "user2", "user3"]}
+
+#about route
+@app.get("/about")
+def about():
+    return {"message": "This is a simple FastAPI application."}
+
+
