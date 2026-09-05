d51c2c8166d6888148223c0498370b7bdc47ec95
Javed Mujawar <mujawar.javadu@gmail.com>
2026-09-05 13:35:43 +0530

    Pydantic Models

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app = FastAPI()
 
+class User(BaseModel):
+    name: str
+    age: int
+    email: str="abc"
 # home route
-@app.get("/users")
-def home(name:str= None):
-    return {"Name": name}
+@app.post("/create-user")
+def create_user(name:str, age:int=0):
+    return {"Name": name, "Age": age}
 
-@app.get("/products")
-def products(limit:int=10):
-    return { "limit": limit}
+@app.post("/create-item")
+def create_item(item:User):
+    return {"message": "User Created", "date": item}
 
-@app.get("/items")
-def items(name:str=None, price:int=0):
-    return { "Name": name, "Price": price }
+class Address(BaseModel):
+    city: str
+    pincode: int
+
+class User(BaseModel):
+    name: str
+    age: int
+    email: str
+    address: Address
+
+@app.post("/create-user-with-address")    
+def create_user_with_address(user: User):
+    return user
