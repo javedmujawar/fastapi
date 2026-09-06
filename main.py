bd842bff3286eb42727983cfeb315ab3a96c05a8
Javed Mujawar <mujawar.javadu@gmail.com>
2026-09-06 12:52:14 +0530

    data read

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request,Depends
+from fastapi import FastAPI, Request,Depends,HTTPException
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker, declarative_base, Session
 
@@ -31,3 +31,16 @@ def create_todo(title:str, db:Session = Depends(get_db)):
     db.commit()
     db.refresh(todo)
     return {"message": "Todo created successfully", "todo": todo}
+#read all todos
+@app.get("/todos")
+def read_todos(db: Session = Depends(get_db)):
+    todos = db.query(Todo).all()
+    return {"todos": todos}
+
+#read all todos
+@app.get("/todos/{todo_id}")
+def read_todo(todo_id:int,db: Session = Depends(get_db)):
+    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if not todo:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return {"todo": todo}
