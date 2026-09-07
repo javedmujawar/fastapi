649f1529d59ac3bd20fef341f5b858258adf13de
Javed Mujawar <mujawar.javadu@gmail.com>
2026-09-07 13:54:18 +0530

    api test

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from config import settings
 
 app = FastAPI()
 
-# allowed origins (Front-end url)
-origins = settings.origins
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.get("/")
 def home():
-    return {"message": "CORS enabled api"}
+    return {"message": "Hello Javed"}
+
+
+@app.get("/add")
+def add(a: int, b: int):
+    return {"result": a + b}
