d706b9946f51af5b221b3212b8e49f93804f3d72
Javed Mujawar <mujawar.javadu@gmail.com>
2026-09-08 10:30:44 +0530

    pagination

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import requests
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from bs4 import BeautifulSoup
 
 app = FastAPI()
 
-@app.get("/news")
-def get_news():
-    url ="https://indianexpress.com/"
-    response = requests.get(url)
-    title=[]
-    soap = BeautifulSoup(response.text,"html.parser")
-    for item in soap.find_all("a",class_="topblockNews__featuredLink"):
-        title.append(item)
 
-    return title    
+@app.get("/news")
+def get_news(page: int = 1, limit: int = 10):
+    url = "https://indianexpress.com/"
+    response = requests.get(url)
+    title = []
+    soap = BeautifulSoup(response.text, "html.parser")
+    for item in soap.find_all("span", class_="titleline"):
+        title.append(item.text)
+    start = (page - 1) * limit
+    end = start + limit
+    return {"page":page, "limit":limit, "total": len(title),"data": title[start,end]}
