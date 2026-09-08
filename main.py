96213a6870192ce01fbb6d14417a0775998b29e6
Javed Mujawar <mujawar.javadu@gmail.com>
2026-09-08 10:04:59 +0530

    Third party api call

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from fastapi import FastAPI
+import requests
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
 
-@app.get("/")
-def home():
-    return {"message": "Hello Javed"}
+@app.get("/posts")
+def get_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    return response.json()
 
 
-@app.get("/add")
-def add(a: int, b: int):
-    return {"result": a + b}
+@app.get("/posts/{post_id}")
+def get_post(post_id: int):
+    response = requests.get(f"https://jsonplaceholder.typicode.com/posts/{post_id}")
+    if response.status_code != 200:
+        raise HTTPException(status_code=404, detail="page not found")
+    return response.json()
